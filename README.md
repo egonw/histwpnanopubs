0778af5d10cbd19c3066926676e80514cb1b8be8
Egon Willighagen <egon.willighagen@gmail.com>
2017-04-12 08:49:01 +0200

    Full design

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 Historic Nanopubs for WikiPathways 
 ==================================
 
+Copy in the WikiPathways Java archive for creating nanopublications
+from https://github.com/wikipathways/nanopublications
+
 Run:
 
     bash createNanopub.sh
--- createNanopubs.sh
+++ createNanopubs.sh
@@ -4,11 +4,22 @@ RELEASES="20160610 \
  20160910 \
  20161010"
 
-for release in ${RELEASES};
-  do echo "${release}";
+SUBSETS="wp1 wp2 wp3 wp4 wp5 wp6 wp7 wp8 wp9"
+
+NANOPUBS="InteractionPubs \
+ ComplexesPubs \
+ PathwayParticipationPubs"
+
+for release in ${RELEASES}; do
+  echo "${release}";
   mkdir -p ${release}
   cd ${release}
   echo "wget -c http://data.wikipathways.org/${release}/rdf/wikipathways-${release}-rdf-wp.zip"
   echo "unzip wikipathways-${release}-rdf-wp.zip"
+  for subset in ${SUBSETS}; do
+    for pubtype in ${NANOPUBS}; do
+      echo "java -DOPSWPRDF=`pwd`/${release} -DSUBSETPREFIX=${subset} -Xms2G -Xmx2G -server -cp wikipathways.nanopubs-1-SNAPSHOT.jar nl.unimaas.bigcat.wikipathways.nanopubs.${pubtype}"
+    done
+  done
   cd ..
 done
