eb981911eac52247487d78ec1455e9d797035034
Egon Willighagen <egon.willighagen@gmail.com>
2017-04-12 09:41:57 +0200

    Trying to get it going

--- createNanopubs.sh
+++ createNanopubs.sh
@@ -1,8 +1,5 @@
 RELEASES="20160610 \
- 20160710 \
- 20160810 \
- 20160910 \
- 20161010"
+ 20160710 "
 
 SUBSETS="wp1 wp2 wp3 wp4 wp5 wp6 wp7 wp8 wp9"
 
@@ -11,14 +8,15 @@ NANOPUBS="InteractionPubs \
  PathwayParticipationPubs"
 
 for release in ${RELEASES}; do
-  echo "${release}";
+  echo "Processing ${release}";
   mkdir -p ${release}
   cd ${release}
-  echo "wget -c http://data.wikipathways.org/${release}/rdf/wikipathways-${release}-rdf-wp.zip"
-  echo "unzip wikipathways-${release}-rdf-wp.zip"
+  wget -c http://data.wikipathways.org/${release}/rdf/wikipathways-${release}-rdf-wp.zip
+  unzip -n wikipathways-${release}-rdf-wp.zip
   for subset in ${SUBSETS}; do
     for pubtype in ${NANOPUBS}; do
-      echo "java -DOPSWPRDF=`pwd`/${release} -DSUBSETPREFIX=${subset} -Xms2G -Xmx2G -server -cp wikipathways.nanopubs-1-SNAPSHOT.jar nl.unimaas.bigcat.wikipathways.nanopubs.${pubtype}"
+      echo `pwd`
+      echo "java -DOPSWPRDF=`pwd` -DSUBSETPREFIX=${subset} -Xms2G -Xmx2G -server -cp ../wikipathways.nanopubs-1-SNAPSHOT.jar nl.unimaas.bigcat.wikipathways.nanopubs.${pubtype}"
     done
   done
   cd ..
